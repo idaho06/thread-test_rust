2e594af50537a431b9410f3ed8a30092c2412f47
idaho06 <cesar.idaho@gmail.com>
2022-12-11 18:51:34 +0100

    moved closure in thread spawn to function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::time;
+use std::sync::mpsc::Sender;
 use std::{sync::mpsc, thread, io};
 use std::io::Write;
 
@@ -10,33 +11,35 @@ enum Messages {
     Quit,
 }
 
+fn event_creator_thread(tx: Sender<Messages>) {
+    let mut current_message = Messages::Message01;
+    loop {
+        thread::sleep(time::Duration::from_secs(1_u64));
+        let message_totx = current_message;
+        tx.send(message_totx).unwrap();
+        match current_message {
+            Messages::Message01 => current_message = Messages::Message02,
+            Messages::Message02 => current_message = Messages::Quit,
+            Messages::Quit => break,
+            _ => (),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, threads!");
     let mut spin: Vec<char> = vec!['|', '/', '-', '\\'];
 
     let (tx, rx) = mpsc::channel::<Messages>();
 
-    let handle = thread::spawn(move || {
-        let mut current_message = Messages::Message01;
-        loop {
-            thread::sleep(time::Duration::from_secs(1_u64));
-            let message_totx = current_message;
-            tx.send(message_totx).unwrap();
-            match current_message {
-                Messages::Message01 => current_message = Messages::Message02,
-                Messages::Message02 => current_message = Messages::Quit,
-                Messages::Quit => break,
-                _ => (),
-            }
-        }
-    });
+    let handle = thread::spawn(move || event_creator_thread(tx));
 
     let mut count = 0_usize;
     loop {
 
         let message = rx.try_recv().unwrap_or(Messages::NoMessage);
         match message {
-            Messages::NoMessage => thread::sleep(time::Duration::from_millis(100)),
+            Messages::NoMessage => thread::sleep(time::Duration::from_millis(1)),
             Messages::Message01 => println!("Message 1!!"),
             Messages::Message02 => println!("Message 2!!"),
             Messages::Quit => break,
